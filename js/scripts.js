baa916d03b9c9b80bc479bddfb03f8ae29199b7a
Drew0004 <drdrewgraphic@gmail.com>
2024-01-09 14:40:13 +0100

    Stampati in pagina tasks

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,18 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-
+      todo:[
+        {
+          task: 'Creare Milestone 1',
+          done: false
+        },        {
+          task: 'Creare Milestone 2',
+          done: false
+        },        {
+          task: 'Creare Milestone 3',
+          done: false
+        },
+      ]
     }
   }
 }).mount('#app')
